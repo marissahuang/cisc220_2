44e038e6d565bcf43e59f23ce8137e49faaea583
Kevin Zuern <kevin@kevinzuern.com>
2016-10-30 19:14:26 -0400

    Added names and fixed some issues

--- firewallSetup.sh
+++ firewallSetup.sh
@@ -1,3 +1,10 @@
+#!/bin/bash
+
+# Deven Bernard     - 10099810
+# Marissa Huang     - 10179169
+# Kevin Zuern       - 10134425
+# Quentin Petraroia - 10145835
+
 # Clear previous rules
 iptables -F INPUT
 iptables -F OUTPUT
@@ -19,13 +26,15 @@ iptables -A INPUT -p TCP --dport 443 -j ACCEPT
 iptables -A OUTPUT -p TCP --dport 80 -j ACCEPT
 iptables -A OUTPUT -p TCP --dport 443 -j ACCEPT
 
-#@ Allow 130.15.100.100 to connect to mysql on 3306
+# Allow 130.15.100.100 to connect to mysql on 3306
 iptables -A INPUT -p TCP -s 130.15.100.100 --dport 3306 -j ACCEPT
 iptables -A OUTPUT -p TCP -s 130.15.100.100 --dport 3306 -j ACCEPT
 
-# Explicity allow connections from 130.15.0.0 to 130.15.255.255
-iptables -A INPUT -p TCP -s 130.15.0.0/16 -j ACCEPT
+# Explicity allow connections out from 130.15.0.0 to 130.15.255.255
 iptables -A OUTPUT -p TCP -s 130.15.0.0/16 -j ACCEPT
 
 # Drop any other outgoing TCP traffic
+iptables -A INPUT -p TCP -j DROP
+iptables -A FORWARD -p TCP -j DROP
 iptables -A OUTPUT -p TCP -j DROP
+
